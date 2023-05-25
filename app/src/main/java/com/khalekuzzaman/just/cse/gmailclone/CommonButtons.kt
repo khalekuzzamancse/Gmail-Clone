fd2b2f66b720b55c11d1e987bfd7caf0611d5001
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-05-25 13:53:12 +0600

    ContextualTopAppbar added

---
+++ app/src/main/java/com/khalekuzzaman/just/cse/gmailclone/CommonButtons.kt
@@ -0,0 +1,42 @@
+package com.khalekuzzaman.just.cse.gmailclone
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+
+@Composable
+fun CommonIconButton(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    onClick: () -> Unit = {},
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun CommonIconButton(
+    modifier: Modifier = Modifier,
+    resourceId: Int,
+    onClick: () -> Unit = {},
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Icon(
+            painter = painterResource(id = resourceId),
+            contentDescription = null
+        )
+    }
+}
--- app/src/main/java/com/khalekuzzaman/just/cse/gmailclone/TopAppbars.kt
+++ app/src/main/java/com/khalekuzzaman/just/cse/gmailclone/TopAppbars.kt
@@ -6,13 +6,17 @@ import androidx.annotation.Px
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,10 +33,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
@@ -81,7 +87,11 @@ fun TopAppbarM3_01() {
 @Composable
 fun CommonTopAppbar(moveAppbarVerticallyBy: Int = 0) {
     CollapseAbleTopAppbar {
-        SearchBar(modifier = Modifier.fillMaxSize().weight(1f))
+        SearchBar(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        )
         ProfileImage(drawableResource = R.drawable.profile_image)
     }
 
@@ -145,6 +155,79 @@ class CollapsableToolbarState(
                 return Offset.Zero
             }
         }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContextualTopAppbar(
+    onBackArrowClick: () -> Unit = {},
+    selectedEmails: Int = 0,
+    onArchiveButtonClick: () -> Unit = {},
+    onDeleteButtonClick: () -> Unit = {},
+    onMarkAsUnReadButtonClick: () -> Unit = {},
+) {
+    TopAppBar(
+        title = {},
+        navigationIcon = {
+            CommonIconButton(
+                imageVector = Icons.Default.ArrowBack,
+                onClick = onBackArrowClick
+            )
+        },
+        actions = {
+            if (selectedEmails > 0) {
+                Text(text = "$selectedEmails")
+            }
+
+            Spacer(modifier = Modifier.width(40.dp))
+            CommonIconButton(
+                resourceId = R.drawable.ic_archive,
+                onClick = onArchiveButtonClick
+            )
+            CommonIconButton(
+                resourceId = R.drawable.ic_delete,
+                onClick = onDeleteButtonClick
+            )
+            CommonIconButton(
+                resourceId = R.drawable.ic_mark_as_unread,
+                onClick = onMarkAsUnReadButtonClick
+            )
+            CommonIconButton(
+                imageVector = Icons.Default.MoreVert,
+                onClick = { }
+            )
+        }
+    )
+}
+
+
+@Composable
+@Preview(showBackground = true)
+private fun ContextualTopAppbarPreview() {
+    ContextualTopAppbar()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(showBackground = false)
+private fun ContextualTopAppbarPreview2() {
+    Scaffold(
+        topBar = {
+            ContextualTopAppbar()
+        }) {
+        LazyColumn(modifier = Modifier.padding(it)) {
+            items(3) { index ->
+                Text(
+                    "I'm item $index", modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
