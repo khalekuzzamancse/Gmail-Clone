4c5b0baf3de4a87dab2d5777c56873573d9d28bf
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-06-07 01:25:50 +0600

    Custom top app bar added for list type screens

--- ui_mobile/src/main/java/com/khalekuzzaman/just/cse/gmailclone/MainActivity.kt
+++ ui_mobile/src/main/java/com/khalekuzzaman/just/cse/gmailclone/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.khalekuzzaman.just.cse.gmailclone.ui.common.CommonScreenWithModalDrawerDemo
 import com.khalekuzzaman.just.cse.gmailclone.ui.common.ListScreenTopAppbarDemo
+import com.khalekuzzaman.just.cse.gmailclone.ui.navigation.ModalDrawerNavHost
 import com.khalekuzzaman.just.cse.gmailclone.ui.theme.GmailCloneTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,10 +16,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GmailCloneTheme {
-                //ModalDrawerNavHost()
+                ModalDrawerNavHost()
                 // EmailListUsingSlotEmailItemDemo()
                // CommonScreenWithModalDrawerDemo()
-                ListScreenTopAppbarDemo()
+             //   ListScreenTopAppbarDemo()
 
             }
         }
--- ui_mobile/src/main/java/com/khalekuzzaman/just/cse/gmailclone/ui/common/CommonScreenWithModalDrawer.kt
+++ ui_mobile/src/main/java/com/khalekuzzaman/just/cse/gmailclone/ui/common/CommonScreenWithModalDrawer.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
+import com.khalekuzzaman.just.cse.gmailclone.R
 import com.khalekuzzaman.just.cse.gmailclone.ui.common.DrawerItemsProvider.drawerGroups
 import com.khalekuzzaman.just.cse.gmailclone.utils.CustomNestedScrollConnection
 import com.khalekuzzaman.just.cse.gmailclone.utils.ScrollDirection
@@ -219,6 +219,12 @@ fun ScreenScaffold(
                         onMarkAsUnReadButtonClick = onMarkAsUnReadButtonClick
                     )
                 } else {
+                    CommonTopAppbarForListScreen(
+                        profileImageResourceId = R.drawable.ic_profile_2,
+                        onNavigationIconClick = {},
+                        onProfileIconClick = {},
+                        onSearchTextClick = {}
+                    )
 
                 }
 
