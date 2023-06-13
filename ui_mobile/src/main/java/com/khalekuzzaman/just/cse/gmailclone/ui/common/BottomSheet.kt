b19b71c37db74ff8880cdcb48d522bc12465c75c
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-06-13 20:10:22 +0600

    BottomSheet added

--- ui_mobile/src/main/java/com/khalekuzzaman/just/cse/gmailclone/ui/common/BottomSheet.kt
+++ ui_mobile/src/main/java/com/khalekuzzaman/just/cse/gmailclone/ui/common/BottomSheet.kt
@@ -10,11 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
@@ -25,9 +26,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.khalekuzzaman.just.cse.gmailclone.R
+import com.khalekuzzaman.just.cse.gmailclone.data.FakeEmailList
 
 @Composable
 @Preview(showBackground = true)
@@ -106,7 +109,7 @@ fun LabelSheet(
     list: List<LabelSheetItem>,
     onChecked: (itemName: String) -> Unit,
 ) {
-    BottomSheetSlot(
+    LabelBottomSheetSlot(
         dismissButton = {
             Icon(
                 modifier = Modifier.padding(start = 24.dp),
@@ -162,7 +165,7 @@ fun LabelItem(
 
     Row(
         modifier
-            .padding(start=24.dp)
+            .padding(start = 24.dp)
             .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
@@ -186,7 +189,7 @@ fun LabelItem(
 
 
 @Composable
-fun BottomSheetSlot(
+fun LabelBottomSheetSlot(
     dismissButton: @Composable () -> Unit,
     title: @Composable () -> Unit,
     searchContent: (@Composable () -> Unit)? = null,
@@ -195,9 +198,12 @@ fun BottomSheetSlot(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)) {
+            .background(Color.White)
+    ) {
         Row(
-            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Box(modifier = Modifier) { dismissButton() }
@@ -212,8 +218,171 @@ fun BottomSheetSlot(
                 Divider()
             }
         }
-        Box(modifier = Modifier.padding(24.dp).fillMaxWidth()) { content() }
+        Box(
+            modifier = Modifier
+                .padding(24.dp)
+                .fillMaxWidth()
+        ) { content() }
     }
 
+
+}
+
+/*
+-
+
+ */
+@Composable
+fun RecipientBottomSheet(
+
+) {
+    LabelBottomSheetSlot(
+        dismissButton = {
+            Icon(
+                modifier = Modifier.padding(start = 24.dp),
+                painter = painterResource(id = R.drawable.ic_cross),
+                contentDescription = null
+            )
+        },
+        title = {
+            Text(
+                style = MaterialTheme.typography.titleLarge,
+                text = "Label"
+            )
+        },
+        searchContent = {
+            Row(modifier = Modifier.height(48.dp)) {
+            }
+        }
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+
+
+        }
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun RecipientBottomSheetSuggestionListPreview() {
+    RecipientBottomSheetSuggestionList(
+        list = FakeEmailList.getFakeEmails()
+    )
+}
+
+@Composable
+fun RecipientBottomSheetSuggestionList(
+    list: List<EmailModel>,
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Suggestions")
+            Spacer(modifier = Modifier.width(4.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.ic_updates),
+                contentDescription = null
+            )
+        }
+        LazyColumn() {
+
+            items(items = list) { item ->
+                RecipientBottomSheetItem(
+                    profileImageResourceId = item.profileImageId,
+                    title = item.receiver,
+                    subTitle = item.receiver,
+                )
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun RecipientBottomSheetItem(
+    profileImageResourceId: Int,
+    title: String,
+    subTitle: String,
+) {
+    RecipientBottomSheetSlot(
+        profileImage = {
+            Icon(
+                painter = painterResource(id = profileImageResourceId),
+                contentDescription = null
+            )
+        },
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        subTitle = {
+            Text(
+                text = subTitle,
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun RecipientBottomSheetSlotPreview() {
+    RecipientBottomSheetSlot(
+        profileImage = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_profile_2),
+                contentDescription = null
+            )
+        },
+        title = {
+            Text(
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = "khalekuzzaman91@gmail.com",
+                style = MaterialTheme.typography.titleLarge
+            )
+        },
+        subTitle = {
+            Text(
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = "khalekuzzaman91@gmail.com",
+                style = MaterialTheme.typography.titleSmall
+            )
+        }
+    )
+}
+
+@Composable
+fun RecipientBottomSheetSlot(
+    modifier: Modifier = Modifier,
+    profileImage: @Composable () -> Unit,
+    title: @Composable () -> Unit,
+    subTitle: @Composable () -> Unit,
+) {
+    val margin = 8.dp
+    Row(
+        modifier = modifier
+            .padding(margin)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(modifier = Modifier.size(48.dp)) { profileImage() }
+        Spacer(modifier = Modifier.width(16.dp))
+        Column(modifier = Modifier.fillMaxWidth()) {
+            title()
+            Spacer(modifier = Modifier.height(8.dp))
+            subTitle()
+        }
+    }
 
 }
